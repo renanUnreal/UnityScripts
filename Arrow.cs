9c8d8c303ac5a77df7c2c4640b18d0f5bc568ff9
renanUnreal <renanunreal@gmail.com>
2019-07-25 16:10:15 -0300

    Script de flecha para game de arco e flecha vr
    este script tem a movimentação de flechas e mecanica de disparo das mesmas
    nao contem a dinamica do arco estas se encontram em um script chamado BOW

--- Arrow.cs
+++ Arrow.cs
@@ -1,9 +1,11 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class Arrow : MonoBehaviour
 {
-
+    // velocidade da flesha
     public float m_Speed = 2000.0f;
+    // parte da flecha que fara a colisao e sera resposavel pela fisica para que ela caia com a frente mais pesada que o corpo 
+    // este item esta no PREFAB da flecha é a ponta dela
     public Transform m_Tip = null;
 
     private Rigidbody m_Rigidbody = null;
@@ -48,9 +50,8 @@ public class Arrow : MonoBehaviour
     {
         m_IsStopped = false;
         transform.parent = null;
-        
-        
-         m_Rigidbody.isKinematic = false;
+
+        m_Rigidbody.isKinematic = false;
         m_Rigidbody.useGravity = true;
         m_Rigidbody.AddForce(transform.forward * (pullValue * m_Speed));
         Destroy(gameObject, 5.0f);
@@ -73,3 +74,4 @@ public class Arrow : MonoBehaviour
 
 
 }
+
