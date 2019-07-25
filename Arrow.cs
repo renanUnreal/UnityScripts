6441d165f347f94f59967c944a4b96b5945033cf
renanUnreal <renanunreal@gmail.com>
2019-07-25 15:41:53 -0300

    Script de Flechas para o sistemna de arco e flecha Vr
    este e o responsavel pelo disparo das flechas do VIVE

---
+++ Arrow.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+
+    public float m_Speed = 2000.0f;
+    public Transform m_Tip = null;
+
+    private Rigidbody m_Rigidbody = null;
+    private bool m_IsStopped = true;
+    private Vector3 m_LastPosition = Vector3.zero;
+
+    private void Awake()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (m_IsStopped)
+        {
+            return;
+        }
+
+        // Rotate
+        
+        m_Rigidbody.MoveRotation(Quaternion.LookRotation(m_Rigidbody.velocity, transform.up));
+
+
+        // Colision
+        if(Physics.Linecast(m_LastPosition, m_Tip.position))
+        {
+            Stop();
+        }
+
+        // Store position
+        m_LastPosition = m_Tip.position;
+    }
+
+    private void Stop()
+    {
+        m_IsStopped = true;
+        m_Rigidbody.isKinematic = true;
+        m_Rigidbody.useGravity = false;
+    }
+
+    public void Fire(float pullValue)
+    {
+        m_IsStopped = false;
+        transform.parent = null;
+
+        m_Rigidbody.useGravity = true;
+        m_Rigidbody.AddForce(transform.forward * (pullValue * m_Speed));
+        Destroy(gameObject, 5.0f);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
