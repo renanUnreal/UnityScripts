3e5accc228ee2ccab56df55938e0851d906faf85
renanUnreal <renanunreal@gmail.com>
2019-09-26 14:46:02 -0300

    scripts conect game
    game de conectar objetos tipo quebra cabeça

---
+++ ChecarEstado.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChecarEstado : MonoBehaviour
+{ 
+ public bool completou;
+float cronometro;
+ public bool game;
+private static int pontos;
+public int cena;
+public float tempo;
+ private float tempoDeJogo;
+public bool final;
+    private float contador;
+public GameObject final1;
+public GameObject final2;
+public bool inicioGame;
+
+ [SerializeField] private TextMesh timeLabel;
+ [SerializeField] private TextMesh totalLabel;
+
+    MoveObject2D[] objetos;
+
+void Start()
+{
+    cronometro = 0;
+    completou = false;
+    objetos = FindObjectsOfType<MoveObject2D>();
+        tempoDeJogo = tempo;
+
+        if (final)
+        {
+            if (pontos > 9)
+            {
+                final1.SetActive(true);
+                final2.SetActive(false);
+            }
+            else
+            {
+                final1.SetActive(false);
+                final2.SetActive(true);
+            }
+        }
+
+        if (inicioGame)
+        {
+            pontos = 0;
+        }
+
+}
+
+void Update()
+{
+        if (game)
+        {
+            tempoDeJogo = tempoDeJogo - Time.deltaTime;
+            timeLabel.text = "Tempo: " + tempoDeJogo.ToString("f0");
+            contador += Time.deltaTime;
+            cronometro += Time.deltaTime;
+            if (cronometro >= 0.2f)
+            { //5Hz
+                cronometro = 0;
+                int soma = 0;
+                for (int x = 0; x < objetos.Length; x++)
+                {
+                    if (objetos[x].estaConectado)
+                    {
+                        soma++;
+                        pontos = soma;
+                        totalLabel.text = "Pontos: " + soma.ToString("f0");
+                    }
+                }
+
+                if (soma >= objetos.Length)
+                {
+                    completou = true;
+                    mudacena(2);
+                }
+            }
+            if (tempo < contador)
+            {
+                mudacena(2);
+            }
+        }
+
+}
+
+   public void mudacena(int x)
+    {
+       SceneManager.LoadScene(x);
+      
+        
+    }
+
+}
---
+++ Misturador.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Misturador : MonoBehaviour
+{ 
+    public GameObject[] pecas;
+    public Vector3[] posicao;
+
+    public int valorMin = 0;
+    public int valorMax = 14;
+    private int contador = 0;
+    public int numeroSorteado;
+    [Space(20)]
+    public List<int> numerosJaSorteados = new List<int>();
+
+    public List<int> numeros = new List<int>();
+
+    private void Awake()
+    {
+        for (int x = valorMin; x <= valorMax; x++)
+        {
+            numeros.Add(x);
+        }
+    }
+
+
+    void Start()
+    {
+      
+
+               
+
+                
+               
+
+
+       
+        }
+
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (numeros.Count > 0)
+        {
+            GerarNumerosAleatorios();
+          
+        }
+        if(contador == valorMax + 1)
+        {
+            
+            ordenar();
+            contador++;
+        }
+        
+    }
+
+
+    void GerarNumerosAleatorios()
+    {
+        int indice = Random.Range(0, numeros.Count);
+        
+        numeroSorteado = numeros[indice];
+        numerosJaSorteados.Add(numeroSorteado);
+        numeros.Remove(numeros[indice]);
+        // Debug.Log(numerosJaSorteados[contador]);
+       
+         
+            contador++;
+   
+       
+    }
+
+    void ordenar()
+    {
+        for (int x = valorMin; x <= valorMax; x++)
+        {
+            pecas[x].transform.position = posicao[numerosJaSorteados[x]];
+        }
+    }
+}
